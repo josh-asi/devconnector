c604f9d70190b5a88468ce5a12c2b21086bc4af5
Josh Asi <josh@Joshs-MacBook-Pro.local>
2019-01-09 18:07:11 +1300

    Add education and experience  complete

--- client/src/components/add-credentials/AddExperience.js
+++ client/src/components/add-credentials/AddExperience.js
@@ -4,6 +4,7 @@ import TextFieldGroup from '../common/TextFieldGroup';
 import TextAreaFieldGroup from '../common/TextAreaFieldGroup';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import { addExperience } from '../../actions/profileActions';
 
 class AddExperience extends Component {
 	constructor(props) {
@@ -21,14 +22,31 @@ class AddExperience extends Component {
 		};
 	}
 
+	componentWillReceiveProps(nextProps) {
+		if (nextProps.errors) {
+			this.setState({
+				errors: nextProps.errors
+			});
+		}
+	}
+
 	onChange = (e) => {
 		this.setState({ [e.target.name]: e.target.value });
 	};
 
 	onSubmit = (e) => {
 		e.preventDefault();
+		const expData = {
+			company: this.state.company,
+			title: this.state.title,
+			location: this.state.location,
+			from: this.state.from,
+			to: this.state.to,
+			current: this.state.current,
+			description: this.state.description
+		};
 
-		console.log('submit');
+		this.props.addExperience(expData, this.props.history);
 	};
 
 	onCheck = (e) => {
@@ -113,7 +131,7 @@ class AddExperience extends Component {
 									value={this.state.description}
 									onChange={this.onChange}
 									error={errors.description}
-									info="Tell us about the the position"
+									info="Tell us about the position"
 								/>
 								<input type="submit" value="Submit" className="btn btn-info btn-block mt-4" />
 							</form>
@@ -136,4 +154,4 @@ const mapStateToProps = (state) => ({
 	errors: state.errors
 });
 
-export default connect(mapStateToProps)(withRouter(AddExperience));
+export default connect(mapStateToProps, { addExperience })(withRouter(AddExperience));
