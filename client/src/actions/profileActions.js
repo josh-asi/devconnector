e592e2ccaf8476d2d367fdcfdd7f0dae48421887
Josh Asi <josh@Joshs-MacBook-Pro.local>
2019-01-10 14:36:40 +1300

    Profile page almost completed. Must finish github

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -113,6 +113,25 @@ export const getProfiles = () => (dispatch) => {
 		);
 };
 
+// Get profile by handle profile
+export const getProfileByHandle = (handle) => (dispatch) => {
+	dispatch(setProfileLoading());
+	axios
+		.get(`/api/profile/handle/${handle}`)
+		.then((res) =>
+			dispatch({
+				type: GET_PROFILE,
+				payload: res.data
+			})
+		)
+		.catch((err) =>
+			dispatch({
+				type: GET_PROFILE,
+				payload: null
+			})
+		);
+};
+
 // Delete account & profile
 export const deleteAccount = () => (dispatch) => {
 	if (window.confirm('Are you sure? This can NOT be undone!')) {
