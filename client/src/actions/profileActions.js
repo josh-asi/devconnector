6df341fc4729c1cf66ad444810225427330549d9
Josh Asi <josh@Joshs-MacBook-Pro.local>
2019-01-10 13:40:07 +1300

    Pulls all profile and displays on /profiles

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -1,6 +1,13 @@
 import axios from 'axios';
 
-import { GET_PROFILE, PROFILE_LOADING, CLEAR_CURRENT_PROFILE, GET_ERRORS, SET_CURRENT_USER } from './types';
+import {
+	GET_PROFILE,
+	GET_PROFILES,
+	PROFILE_LOADING,
+	CLEAR_CURRENT_PROFILE,
+	GET_ERRORS,
+	SET_CURRENT_USER
+} from './types';
 
 // Get current profile
 export const getCurrentProfile = () => (dispatch) => {
@@ -83,6 +90,25 @@ export const deleteEducation = (id) => (dispatch) => {
 			dispatch({
 				type: GET_ERRORS,
 				payload: err.response.data
+			})
+		);
+};
+
+// Get all profiles
+export const getProfiles = () => (dispatch) => {
+	dispatch(setProfileLoading());
+	axios
+		.get('/api/profile/all')
+		.then((res) =>
+			dispatch({
+				type: GET_PROFILES,
+				payload: res.data
+			})
+		)
+		.catch((err) =>
+			dispatch({
+				type: GET_PROFILES,
+				payload: null
 			})
 		);
 };
