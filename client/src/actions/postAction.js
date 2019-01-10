431c71e46e755e8555112996eebd361d955e1a1d
Josh Asi <josh@Joshs-MacBook-Pro.local>
2019-01-10 17:32:53 +1300

    Delete posts

--- client/src/actions/postAction.js
+++ client/src/actions/postAction.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-import { ADD_POST, GET_POSTS, GET_ERRORS, POST_LOADING } from './types';
+import { ADD_POST, GET_POSTS, GET_ERRORS, POST_LOADING, DELETE_POST } from './types';
 
 // Add Post
 export const addPost = (postData) => (dispatch) => {
@@ -35,6 +35,24 @@ export const getPosts = () => (dispatch) => {
 			dispatch({
 				type: GET_POSTS,
 				payload: null
+			})
+		);
+};
+
+// Delete Post
+export const deletePost = (id) => (dispatch) => {
+	axios
+		.delete(`/api/posts/${id}`)
+		.then((res) =>
+			dispatch({
+				type: DELETE_POST,
+				payload: id
+			})
+		)
+		.catch((err) =>
+			dispatch({
+				type: GET_ERRORS,
+				payload: err.response.data
 			})
 		);
 };
